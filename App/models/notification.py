a38d826d768d4cdcda4c213042ce413bef5df232
Jade-Ganga <jonathon.wingson@my.uwi.edu>
2022-10-05 13:35:12 +0000

    relationship changes

--- App/models/notification.py
+++ App/models/notification.py
@@ -3,14 +3,20 @@ from App.database import db
 
 class Notification(db.Model):                             
     id = db.Column(db.Integer, primary_key=True)
+    reciever_id = db.Column(db.Interger, nullable=False)
+    sender_id = db.Column(db.Interger, nullable=False)
     message = db.Column(db.String(200), nullable=False)
 
-    def _init_(self, id, message, StudentID, StaffID):
+    def _init_(self, id, message, reciever_id, sender_id):
         self.ID = ID
         self.message = message
+        self.reciever_id = reciever_id
+        self.sender_id = sender_id
 
     def toJSON(self):
         return{
             'id': self.id,
-            'message': self.message
+            'message': self.message,
+            'reciever_id': self.reciever_id,
+            'sender_id': self.sender_id
         }
--- App/models/recommendation.py
+++ App/models/recommendation.py
@@ -3,14 +3,17 @@ from App.database import db
 
 class Recommendation(db.Model):                              #check relationship !!!!!!!!!!!!!!!!!
     id = db.Column(db.Integer, primary_key=True)
+    recipient_id = db.Column(db.Interger,nullable=False)
     letter = db.Column(db.String(200), nullable=False)
 
-    def _init_(self, ID, Letter, StudentID, StaffID):
+    def _init_(self, ID, Letter, recipient_id):
         self.ID = ID
         self.Letter = Letter
+        self.recipient_id = recipient_id
 
     def toJSON(self):
         return{
             'ID': self.ID,
-            'Letter': self.Letter
+            'Letter': self.Letter,
+            'recipient_id': self.recipient_id
         }
