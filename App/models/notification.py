82612ca1c03fc4e9beda36e38ec4df1d38d4f82d
Jade-Ganga <jonathon.wingson@my.uwi.edu>
2022-10-06 22:54:50 +0000

    notification model

--- App/models/notification.py
+++ App/models/notification.py
@@ -1,22 +1,22 @@
 from werkzeug.security import check_password_hash, generate_password_hash
 from App.database import db
 
-class Recommendation(db.Model):                             
+class Notification(db.Model):                      
     id = db.Column(db.Integer, primary_key=True)
     recipient_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    sender = db.Column(db.Integer, nullable=False)                                      ##check if it needs to be forieggn
-    letter = db.Column(db.String(200), nullable=False)
+    sender = db.Column(db.Integer, nullable=False)
+    message = db.Column(db.String(200), nullable=False)
 
-    def _init_(self, ID, Letter, recipient_id):
+    def _init_(self, ID, message, recipient_id):
         self.id = ID                                #for the rec
-        self.letter = Letter                        #message itself
+        self.message = message                        #message itself
         self.recipient_id = recipient_id            #who it is for
         self.sender
 
     def toJSON(self):
         return{
             'ID': self.id,
-            'Letter': self.letter,
+            'message': self.message,
             'recipient_id': self.recipient_id,
             'sender': self.sender
         }
